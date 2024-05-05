41a835be2ac869f32af72709a7af0e653e18a923
Harsh Seth <30751873+harsh-seth@users.noreply.github.com>
2024-05-04 21:54:37 -0400

    fix merge issues

--- PyPokerEngine/pypokerengine/pokeragent.py
+++ PyPokerEngine/pypokerengine/pokeragent.py
@@ -51,8 +51,8 @@ class PokerAgent(BasePokerPlayer):
       round=round,
       k=k,
       action_history=action_history,
-      aggression=0
-      raise_count=raise_count
+      aggression=0,
+      raise_count=raise_count,
     )
     action = tree.pick_Action() # Returns "Optimal" move: 0 = Fold, 1 = Call, 2 = Raise
     move = valid_actions[action]
