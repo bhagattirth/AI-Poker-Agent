a22f0a7581c701d2affc42ae6a13c6ee8b11f87e
Harsh Seth <hseth@umass.edu>
2024-05-08 15:40:11 -0400

    feat: Quieter logs

--- PyPokerEngine/example.py
+++ PyPokerEngine/example.py
@@ -10,19 +10,25 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--rounds', type=int, default=50)
 parser.add_argument('--stack', type=int, default=1000)
 parser.add_argument('--interactive', '-i', action='store_true')
+parser.add_argument('--data', '-d', action='store_true')
+parser.add_argument('--verbose', '-v', action='store_true')
 args = parser.parse_args()
 
 #TODO:config the config as our wish
 config = setup_config(max_round=args.rounds, initial_stack=args.stack, small_blind_amount=10)
 
 
+pokerman = PokerAgent(verbose=args.verbose)
 
 if args.interactive:
     config.register_player(name="cli", algorithm=ConsolePlayer())
 else:
     config.register_player(name="Honest", algorithm=HonestPlayer())
 
-config.register_player(name="PokerMan", algorithm=PokerAgent())
+config.register_player(name="PokerMan", algorithm=pokerman)
 
+if args.data:
+    pokerman.report_stats()
+    pokerman.save_stats()
 
 game_result = start_poker(config, verbose=1)
