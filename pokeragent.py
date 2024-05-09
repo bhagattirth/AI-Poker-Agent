6400dd2a0a9015376a81496d5cfbe7aa94ee5ee4
Harsh Seth <hseth@umass.edu>
2024-05-08 20:21:22 -0400

    feat: Same great taste, now with reduced fat!

--- pokeragent.py
+++ pokeragent.py
@@ -1,20 +1,35 @@
-from itertools import combinations
+import pickle
 import time
-from phevaluator.evaluator import evaluate_cards
-import numpy as np
 from math import inf
+from itertools import combinations
+from statistics import variance
+
+import numpy as np
+
+from pypokerengine.players import BasePokerPlayer
+from phevaluator.evaluator import evaluate_cards
+
+stats = []
+starting_stack = 0
+raising_odds = 1
+call_odds = 1
+raise_odds = 1
+sum_val = 3
+stack_history = []
 
 def calculate_hand_strength(hand, river):
     # Given hand cards ('hand', 2 cards) and community cards ('river', up to 5 cards)
     # Determine the holder's hand strength
-    start_time=time.time()
-    all_cards = ['C2', 'D2', 'H2', 'S2', 'C3', 'D3', 'H3', 'S3', 
-                        'C4', 'D4', 'H4', 'S4', 'C5', 'D5', 'H5', 'S5', 
-                        'C6', 'D6', 'H6', 'S6', 'C7', 'D7', 'H7', 'S7',
-                        'C8', 'D8', 'H8', 'S8', 'C9', 'D9', 'H9', 'S9', 
-                        'CT', 'DT', 'HT', 'ST', 'CJ', 'DJ', 'HJ', 'SJ',
-                        'CQ', 'DQ', 'HQ', 'SQ', 'CK', 'DK', 'HK', 'SK', 
-                        'CA', 'DA', 'HA', 'SA']
+    all_cards = [
+        'C2', 'D2', 'H2', 'S2', 'C3', 'D3', 'H3', 'S3', 
+        'C4', 'D4', 'H4', 'S4', 'C5', 'D5', 'H5', 'S5', 
+        'C6', 'D6', 'H6', 'S6', 'C7', 'D7', 'H7', 'S7',
+        'C8', 'D8', 'H8', 'S8', 'C9', 'D9', 'H9', 'S9', 
+        'CT', 'DT', 'HT', 'ST', 'CJ', 'DJ', 'HJ', 'SJ',
+        'CQ', 'DQ', 'HQ', 'SQ', 'CK', 'DK', 'HK', 'SK', 
+        'CA', 'DA', 'HA', 'SA'
+    ]
+
     card_eval={}
     opp_eval={}
     for card in hand:
@@ -29,6 +44,7 @@ def calculate_hand_strength(hand, river):
                 for k in range(j+1,len(all_cards)):
                     card_value=evaluate_cards(hand[0][::-1],hand[1][::-1], all_cards[i][::-1], all_cards[j][::-1], all_cards[k][::-1])
                     card_eval[card_value]=card_eval.get(card_value,0)+1
+
     elif len(river)==3:
         for i in range(len(all_cards)):
             for j in range(i+1,len(all_cards)):
@@ -50,6 +66,7 @@ def calculate_hand_strength(hand, river):
             for j in range(i+1,len(all_cards)):
                 card_value=evaluate_cards(hand[0][::-1],hand[1][::-1],river[0][::-1],river[1][::-1],river[2][::-1],river[3][::-1],all_cards[i][::-1])
                 card_eval[card_value]=card_eval.get(card_value,0)+1
+    
     elif len(river)==5:
         for i in range(len(all_cards)):
             for j in range(i+1,len(all_cards)):
@@ -59,6 +76,7 @@ def calculate_hand_strength(hand, river):
         card_eval[card_value]=card_eval.get(card_value,0)+1
 
     max_theoretical_hand_strength = 7462
+    
     # PokerMan Value calculation
     value=0
     for i,v in card_eval.items():
@@ -73,7 +91,6 @@ def calculate_hand_strength(hand, river):
         variance= (np.power(i_norm-value,2)*v)+variance
     std_dev=np.sqrt(variance)
     if len(card_eval)>0:std_dev=std_dev/sum(card_eval.values())
-
 
     # Opponent Value Calculation:
     opp_value=0
@@ -92,9 +109,6 @@ def calculate_hand_strength(hand, river):
     return value, opp_value, std_dev, opp_std_dev
 
 def determine_victory(params, risk_level='high'):
-    # TODO: Account for bluffing
-    # TODO: Account for past results
-
     hypers = {}
     # critical val: determines confidence levels and is expressed as overlap between possible hand values between us and them. higher is more unlikely, 0 is exact values (mean/calc)
     # betting power threshold: an exponential curve from (-inf, 1) where 0 => equal wealth, 1 => complete monopoly, -inf => bankruptcy
@@ -133,6 +147,72 @@ def determine_victory(params, risk_level='high'):
                 or params['num_round_raises'] <= hypers[risk_level]['round_raise_threshold'] # or not be in a high stakes round
             )
     )
+
+class Tree:
+    def __init__(self, position, hand, river, call_amount, raise_amount, p1Money, p2Money, pot, round, k, action_history, raise_count=0, p2_dist=[1,1,1]):
+        self.round = round                      
+
+        p1_hand_strength, p2_hand_strength, p1_hand_strength_rmse, p2_hand_strength_rmse = calculate_hand_strength(hand, river) # Note: hand will always be our (player 1's) cards
+        self.root = Node(
+            position=position,
+            hand=hand,
+            river=river,
+            betting_amount=(call_amount, raise_amount),
+            player_money=(p1Money, p2Money, pot),
+            round=round,
+            k=k,
+            action_history=action_history,
+            owner=1,
+            action=action_history[-1][1],
+            raise_count=raise_count,
+            p1_hand_strength=p1_hand_strength,
+            p2_hand_strength=p2_hand_strength,
+            p1_hand_strength_rmse=p1_hand_strength_rmse,
+            p2_hand_strength_rmse=p2_hand_strength_rmse,
+            p2_dist=p2_dist
+        ) # Current State
+
+    def pick_Action(self) -> int:
+        """
+        Pick_action returns the best action for a given state based on the utility of the move
+        return: returns 0 for fold, 1 for call, 2 for raise
+        """
+
+        best_action = None                            # best action
+        best_utility = float("-inf")                  # best utility
+        for action, state in self.root.get_actions(): # Pick the action with the highest Utility
+            utility = self.get_move_utility(state)    # Get the utility of the move
+            if utility > best_utility:
+                best_action = action
+                best_utility = utility
+        return best_action
+
+    def get_move_utility(self, state, level=1) -> float:
+        """
+        get_move_utility returns the utility of a action using minimax algorithm
+        :state: current state of the game (a Node)
+        :level: current level of the game tree
+        return: returns the utility of the move
+        """
+        is_preflop = self.round == 1
+        # print(state.weight)
+        if state.is_leaf_node():    # If the state is a terminal node return the utility of the state
+            return state.get_utility(is_preflop=is_preflop)
+
+        actions = state.get_actions(is_preflop=is_preflop)
+
+        if not actions:
+            return state.get_utility(is_preflop=is_preflop)
+
+        utils = [nState.weight * self.get_move_utility(nState, level+1) for _, nState in actions] # Recursively get the utility of future moves
+
+        if state.owner == 1:
+            return max(utils)
+        elif state.owner == 2:
+            return sum(utils)/len(utils)
+        else:
+            return sum(utils)/len(utils)
+
 
 class Node:
     def __init__(self, position, hand, river, betting_amount, player_money, round, k, action_history,owner, action, leaf=False, curr_level=0, raise_count=0, p1_hand_strength=None, p2_hand_strength=None, p1_hand_strength_rmse=None, p2_hand_strength_rmse=None, p2_dist=None, weight=1):
@@ -371,8 +451,6 @@ class Node:
 
         return moves
 
-
-
     def action_helper_nature(self, next):
         """
         action_helper_nature returns valid states for nature
@@ -445,3 +523,192 @@ class Node:
                 )))
 
         return moves
+
+
+class PokerAgent(BasePokerPlayer):
+  def __init__(self, verbose=False):
+    super().__init__()
+    self.verbose = verbose
+  
+  def print(self, *args):
+    if self.verbose:
+      print(*args)
+
+  def declare_action(self, valid_actions, hole_card, round_state):
+    start = time.time()
+    position = 0 if round_state["next_player"] == round_state["small_blind_pos"] else 1 # 0 = Small Blind, 1 = Big Blind
+    street = round_state["street"]  # Name of the round
+    if street == "preflop":
+      round = 1
+    elif street == "flop":
+      round = 2
+    elif street == "turn":
+      round = 3
+    elif street == "river":
+      round = 4
+
+    community_card = round_state["community_card"]                                        # River Cards
+    p1_info = round_state["seats"][round_state["next_player"]]
+    p2_info = round_state["seats"][1 if round_state["next_player"] == 0 else 0]
+    p1_money = p1_info["stack"]                                                           # PokerAgent Money
+    p2_money = p2_info["stack"]                                                           # Opponent Money
+    pot = round_state["pot"]["main"]["amount"]                                            # Pot Amount
+    call_amount = 0 if valid_actions[1]["amount"] == 0 else 10                            # Call Amount
+    raise_amount = 10 if call_amount == 0 else 20                                         # Raise Amount
+    raise_count = 0
+
+    for item in round_state['action_histories'][street]:
+      if item['action'] == 'RAISE':
+        raise_count += 1
+
+    k = 3                                                                                 # Depth Limit
+    action_history = [(
+          1 if item['uuid'] == p1_info['uuid'] else 2,
+          item['action'],
+          item['paid'] if 'paid' in item else item['add_amount']
+      ) for (k,v) in round_state['action_histories'].items() for item in v]               # Action History
+
+    global raising_odds
+    global call_odds
+    global raise_odds
+    global sum_val
+
+    last_actions = action_history[-1]
+    if last_actions[1] == 'RAISE' and last_actions[0] == 2:
+      raise_odds += 1
+      sum_val += 1
+    elif last_actions[1] == 'CALL' and last_actions[0] == 2:
+      call_odds += 1
+      sum_val += 1
+    elif last_actions[1] == 'RAISE' and last_actions[0] == 1:
+      raising_odds += 1
+      sum_val += 1
+
+    p2_dist = np.array([raising_odds/(sum_val), call_odds/(sum_val), raise_odds/(sum_val)])
+
+    # p2_dist = np.array([raising_odds/(raising_odds + call_odds + raise_odds + 3), 
+    #            call_odds/(raising_odds + call_odds + raise_odds + 3), 
+    #            raise_odds/(raising_odds + call_odds + raise_odds + 3)])
+    
+
+    tree = Tree(
+      position=position,
+      hand=hole_card,
+      river=community_card,
+      call_amount=call_amount,
+      raise_amount=raise_amount,
+      p1Money=p1_money,
+      p2Money=p2_money,
+      pot=pot,
+      round=round,
+      k=k,
+      action_history=action_history,
+      raise_count=raise_count,
+      p2_dist=p2_dist
+    )
+    action = tree.pick_Action() # Returns "Optimal" move: 0 = Fold, 1 = Call, 2 = Raise
+    move = valid_actions[action]
+
+
+    end = time.time()
+    print("Time taken: ", end - start)
+
+    stats[-1]['time_taken'] = end - start
+    stats[-1]['decisions'].append(move["action"])
+
+    if action == 2: # Raise
+      return move["action"], move["amount"]["min"]
+
+    return move["action"], move["amount"]
+
+  def receive_game_start_message(self, game_info):
+    global stack_history
+    stack_history.append(game_info['rule']['initial_stack'])
+
+  def receive_round_start_message(self, round_count, hole_card, seats):
+    stats.append({
+      'time_taken': None,
+      'decisions': [],
+      'outcome': None,
+      'hand': hole_card,
+      'stack': None,
+    })
+
+    print(f'\n\n-+-+-[Round {round_count}]-+-+-\n')
+    self.print("Cards in hand: ", hole_card)
+
+  def receive_street_start_message(self, street, round_state):
+    self.print(f'\n--[{street}]--')
+
+  def receive_game_update_message(self, action, round_state):
+    pass
+
+  def receive_round_result_message(self, winners, hand_info, round_state):
+    stats[-1]['outcome'] = winners[0]['name'] == 'PokerMan'
+    stats[-1]['river'] = round_state['community_card']
+    stack_history.append([seat['stack'] for seat in round_state['seats'] if seat['name'] == 'PokerMan'][0])
+    stats[-1]['stack'] = stack_history[-1]
+
+    p1_hand_str, p2_hand_str, _, p2_hand_str_rmse = calculate_hand_strength(stats[-1]['hand'], stats[-1]['river'])
+    stats[-1]['hand_strength'] = p1_hand_str
+    stats[-1]['opp_hand_strength'] = p2_hand_str
+    stats[-1]['opp_hand_strength_rmse'] = p2_hand_str_rmse
+
+    action_history = [(
+          1 if item['uuid'] == round_state["seats"][round_state["next_player"]]['uuid'] else 2,
+          item['action'],
+          item['paid'] if 'paid' in item else item['add_amount'] if 'add_amount' in item else 0.0
+      ) for (k,v) in round_state['action_histories'].items() for item in v]
+
+    self.print(f"\nHand strength this round\n  Ours: {stats[-1]['hand_strength']}\nTheirs: {stats[-1]['opp_hand_strength']} (rmse: {stats[-1]['opp_hand_strength_rmse']})")
+    self.print(f"\nPlay this round (player, move, amount added) => {action_history}")
+    self.print(f"\nWe've earned ${stack_history[-1] - stack_history[0]} so far!")
+
+    outcomes = [stat['outcome'] for stat in stats]
+    winnings = [stack_history[idx + 1] - stack_history[idx] for idx, outcome in enumerate(outcomes) if outcome]
+    losses = [stack_history[idx + 1] - stack_history[idx] for idx, outcome in enumerate(outcomes) if not outcome]
+
+    print(f"Average Winnings: ${(sum(winnings)/len(winnings)) if len(winnings) else 0.0} in {len(winnings)} rounds (higher is better)")
+    print(f"Average Losses: ${(sum(losses)/len(losses)) if len(losses) else 0.0} in {len(losses)} rounds (lower is better)")
+
+
+  def report_stats(self):
+    outcomes = [stat['outcome'] for stat in stats]
+
+    won_rounds_hand_strengths = [stats[idx]['hand_strength'] for idx, outcome in enumerate(outcomes) if outcome]
+    won_rounds_opp_hand_strengths = [stats[idx]['opp_hand_strength'] for idx, outcome in enumerate(outcomes) if outcome]
+    won_rounds_opp_hand_strengths_rmse = [stats[idx]['opp_hand_strength_rmse'] for idx, outcome in enumerate(outcomes) if outcome]
+    lost_rounds_hand_strengths = [stats[idx]['hand_strength'] for idx, outcome in enumerate(outcomes) if not outcome]
+    lost_rounds_opp_hand_strengths = [stats[idx]['opp_hand_strength'] for idx, outcome in enumerate(outcomes) if not outcome]
+    lost_rounds_opp_hand_strengths_rmse = [stats[idx]['opp_hand_strength_rmse'] for idx, outcome in enumerate(outcomes) if not outcome]
+
+    if len(won_rounds_hand_strengths) > 1: # variance fn needs at least 2 data points
+      average_won_hand_strength = sum(won_rounds_hand_strengths)/len(won_rounds_hand_strengths)
+      won_hand_strength_var = variance(won_rounds_hand_strengths, average_won_hand_strength)
+
+      average_won_opp_hand_strength = sum(won_rounds_opp_hand_strengths)/len(won_rounds_hand_strengths)
+      won_opp_hand_strength_var = variance(won_rounds_opp_hand_strengths, average_won_opp_hand_strength)
+      
+      average_won_opp_hand_strength_rmse = sum(won_rounds_opp_hand_strengths_rmse)/len(won_rounds_hand_strengths)
+      won_opp_hand_strength_rmse_var = variance(won_rounds_opp_hand_strengths_rmse, average_won_opp_hand_strength_rmse)
+      print("\nHand Strength Stats [Won] (average)")
+      print(f"ours: {average_won_hand_strength} (Var: {won_hand_strength_var}, higher is better)")
+      print(f" opp: {average_won_opp_hand_strength} (Var: {won_opp_hand_strength_var}) [rmse: {average_won_opp_hand_strength_rmse} (Var: {won_opp_hand_strength_rmse_var})]")
+
+    if len(lost_rounds_hand_strengths) > 1: # variance fn needs at least 2 data points
+      average_lost_hand_strength = sum(lost_rounds_hand_strengths)/len(lost_rounds_hand_strengths)
+      lost_hand_strength_var = variance(lost_rounds_hand_strengths, average_lost_hand_strength)
+
+      average_opp_hand_strength = sum(lost_rounds_opp_hand_strengths)/len(lost_rounds_hand_strengths)
+      opp_hand_strength_var = variance(lost_rounds_opp_hand_strengths, average_opp_hand_strength)
+
+      average_lost_opp_hand_strength_rmse = sum(lost_rounds_opp_hand_strengths_rmse)/len(lost_rounds_hand_strengths)
+      lost_opp_hand_strength_rmse_var = variance(lost_rounds_opp_hand_strengths_rmse, average_lost_opp_hand_strength_rmse)
+
+      print("\nHand Strength Stats [Lost] (average)")
+      print(f"ours: {average_lost_hand_strength} (Var: {lost_hand_strength_var}, lower is better)")
+      print(f" opp: {average_opp_hand_strength} (Var: {opp_hand_strength_var}) [rmse: {average_lost_opp_hand_strength_rmse} (Var: {lost_opp_hand_strength_rmse_var})]")
+      
+  def save_stats(self, filename='stats.pkl'):
+    with open(filename, 'wb') as file:
+      pickle.dump(stats, file)
